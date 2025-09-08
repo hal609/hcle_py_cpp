86d19178e0ba3b4f0d432c2b0db967efd39f4fdf
Hal Kolb <hal@kolb.co.uk>
2025-09-08 22:43:12 +0100

    Also remove extra copy in single environment

--- src/hcle/python/hcle_py/env.py
+++ src/hcle/python/hcle_py/env.py
@@ -1,52 +1,81 @@
 import gymnasium as gym
-from gymnasium import spaces 
+from gymnasium import spaces
 import numpy as np
-from . import _hcle_py # Import the C++ module built by pybind11
+from . import _hcle_py  # Import the C++ module built by pybind11
+
 
 class HCLEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 60}
 
-    def __init__(self, game: str, rom_path: str, render_mode: str = "rgb_array", img_height = 240, img_width=256, frame_skip=4, maxpool=False, grayscale=False, stack_num=1, render_fps_limit=0):
-        self.hcle = _hcle_py.PreprocessedEnv(rom_path, game, img_height, img_width, frame_skip, maxpool, grayscale, stack_num)
+    def __init__(
+        self,
+        game: str,
+        rom_path: str,
+        render_mode: str = "rgb_array",
+        img_height=240,
+        img_width=256,
+        frame_skip=4,
+        maxpool=False,
+        grayscale=False,
+        stack_num=1,
+        render_fps_limit=0,
+    ):
+        self.hcle = _hcle_py.PreprocessedEnv(
+            rom_path,
+            game,
+            img_height,
+            img_width,
+            frame_skip,
+            maxpool,
+            grayscale,
+            stack_num,
+        )
 
         self._action_set = self.hcle.get_action_set()
         self.action_space = spaces.Discrete(len(self._action_set))
 
-        single_obs_shape = (stack_num, img_height, img_width) if grayscale else (stack_num, img_height, img_width, (1 if grayscale else 3))
+        single_obs_shape = (
+            (stack_num, img_height, img_width)
+            if grayscale
+            else (stack_num, img_height, img_width, (1 if grayscale else 3))
+        )
         self.observation_space = spaces.Box(
             low=0, high=255, shape=single_obs_shape, dtype=np.uint8
         )
         # self.observation_space = spaces.Box(low=0, high=255, shape=(img_height, img_width, (1 if grayscale else 3)), dtype=np.uint8)
 
-        self.obs_buffer = np.zeros(self.observation_space.shape, dtype=self.observation_space.dtype)
+        self.obs_buffer = np.zeros(
+            self.observation_space.shape, dtype=self.observation_space.dtype
+        )
 
         self.render_mode = render_mode
-        if self.render_mode ==  "human":
+        if self.render_mode == "human":
             self.hcle.create_window(render_fps_limit)
-    
+
     def step(self, action_index: int):
         self.hcle.step(action_index, self.obs_buffer)
         reward = self.hcle.get_reward()
         done = self.hcle.is_done()
-        if done: self.hcle.reset(self.obs_buffer)
+        if done:
+            self.hcle.reset(self.obs_buffer)
         truncated = False
         info = {}
 
-        return np.copy(self.obs_buffer), reward, done, truncated, info
+        return self.obs_buffer, reward, done, truncated, info
 
     def reset(self, *, seed=None, options=None):
-        super().reset(seed=seed) # Important for seeding in Gym
-        
+        super().reset(seed=seed)  # Important for seeding in Gym
+
         self.hcle.reset(self.obs_buffer)
         info = {}
-        return np.copy(self.obs_buffer), info
-        
+        return self.obs_buffer, info
+
     def close(self):
         # Add any C++ cleanup here if necessary
         pass
 
-    def save_to_state(self, state_num:int):
+    def save_to_state(self, state_num: int):
         self.hcle.save_to_state(state_num)
 
-    def load_from_state(self, state_num:int):
+    def load_from_state(self, state_num: int):
         self.hcle.load_from_state(state_num)
